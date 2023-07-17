df23a8380ee4838f8c0cfa088dca034bea47983f
Tossie23 <tossiechr@gmail.com>
2023-07-17 06:33:12 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - print a line to the standard error
+ * main -C program to print a line to the standard error
  * Return: 1 (Successful)
  */
 int main(void)
