4bc192990f3c22036c0cfb3d5b6fcc786363d7f2
Tossie23 <tossiechr@gmail.com>
2023-07-17 06:25:32 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - program print a line to the standard error
+ * main - print a line to the standard error
  * Return: 1 (Successful)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\"
+	\ - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
