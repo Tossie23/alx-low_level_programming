0b93cc43598b7a7f28d3b670b11aa8ae4ba22e01
Tossie23 <tossiechr@gmail.com>
2023-07-18 08:54:44 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,17 +14,17 @@ int main(void)
 		{
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
-			putchar('\t');
+			putchar(' ');
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
 
-			if (!((i == 98) && (j == 99)))
+			if (i != 98 || j != 99)
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
-
+	putchar('\n');
 	return (0);
 }
