1bcade222b9cba74586fccf9b3b63643eb5e2e0f
Tossie23 <tossiechr@gmail.com>
2023-07-18 04:13:32 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,20 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
  * main - Start here, prints single digit numbers of base ten without char
  *
  * Return: 0 (Successful)
  */
-
 int main(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
-	write(1, &i, 1);
+		putchar(i + '0');
 	}
-	write(1, "\n", 1);
+
+	putchar('\n');
 
 	return (0);
 }
