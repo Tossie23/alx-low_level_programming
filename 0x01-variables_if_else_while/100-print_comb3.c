989f76e47c130200495c1ea8a9dc2c4df13a6eb4
Tossie23 <tossiechr@gmail.com>
2023-07-18 05:41:47 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,23 +9,22 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i = 8; i++)
 	{
-		for (j = i + 1; j < 10; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
+		if (i != 0)
+			{
 			putchar(i + '0');
+			}
 			putchar(j + '0');
 
-			if (i < 9 || j < 8)
+			if (i != 8 || j != 9)
 			{
 				putchar(',');
 				putchar(' ');
 			}
 
-			if (j == 9)
-			{
-				i++;
-				j = i;
 			}
 		}
 	}
