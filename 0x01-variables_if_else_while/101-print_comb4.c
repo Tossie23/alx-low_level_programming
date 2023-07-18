784295a7972f7bbbe10cc90f6e2683e74f73727f
Tossie23 <tossiechr@gmail.com>
2023-07-18 08:23:54 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Start here
+ *
+ * Description: Prints only the smallest combinations of three digits
+ *
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 7; i++)
+	{
+	for (j = i + 1; j <= 8; j++)
+	{
+	for (k = j + 1; k <= 9; k++)
+	{
+	putchar((i % 8) + '0');
+	putchar((j % 8) + '0');
+	putchar((k % 8) + '0');
+
+	if (i != 7 || j != 8 || k != 9)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
