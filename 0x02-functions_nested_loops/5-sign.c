bda1808b89cd6ebd1deb34cd7a03e1e37bf0b90d
Tossie23 <tossiechr@gmail.com>
2023-07-21 12:29:14 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,22 +1,12 @@
 #include "main.h"
 
 /**
- * _putchar - Prints a character to stdout
- * @c: The character to be printed
- *
- * Return: On success 1, on error, -1 is returned, errno set appropriately.
- */
-void _putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-/**
  * print_sign - Prints the sign of a number
  * @n: The number to check
  *
  * Return: 1 if n is > zero, 0 if n is zero, -1 if n is < zero
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
