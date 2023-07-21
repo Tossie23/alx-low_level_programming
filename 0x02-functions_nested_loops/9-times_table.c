1ee91d4dc7b3d2eb6d4359f7ab00a986dd27d010
Tossie23 <tossiechr@gmail.com>
2023-07-21 14:25:43 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,7 +3,6 @@
 /**
  * times_table - Prints the 9 times table, starting with 0
  */
-
 void times_table(void)
 {
 	int row, column, product;
@@ -15,9 +14,16 @@ void times_table(void)
 			product = row * column;
 
 			if (product < 10)
+			{
+				if (column != 0)
+					_putchar(' ');
 				_putchar(' ');
+			}
 			else
+			{
+				_putchar(' ');
 				_putchar((product / 10) + '0');
+			}
 
 			_putchar((product % 10) + '0');
 
