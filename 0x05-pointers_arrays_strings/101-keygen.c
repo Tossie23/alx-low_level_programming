3ecef81ee05c1ec9be41a0b3b944cee988588976
Tossie23 <tossiechr@gmail.com>
2023-07-24 13:45:18 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- * main - generates keygen.
- * Return: 0 (Successful)
+ * main - Generates keygen.
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	int r = 0, c = 0;
-	time_t t;
+    int c = 0;
+    time_t t;
 
-	srand((unsigned int) time(&t));
-	while (c < 2772)
-	{
-		r = rand() % 128;
-		if ((c + r) > 2772)
-			break;
-		c = c + r;
-		printf("%c", r);
-	}
-	printf("%c\n", (2772 - c));
-	return (0);
+    srand((unsigned int)time(&t));
+
+    while (c < 2772)
+    {
+        int r = rand() % 128;
+        if ((c + r) > 2772)
+        {
+            break;
+        }
+        c = c + r;
+        putchar(r);
+    }
+
+    putchar(2772 - c);
+    putchar('\n');
+
+    return (0);
 }
