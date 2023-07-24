dc169da7378611bee91f2eaf1d56ab1eb00c13a5
Tossie23 <tossiechr@gmail.com>
2023-07-24 05:54:07 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,14 +7,13 @@
 
 void puts2(char *str)
 {
-        int i = 0;
+	int i = 0;
 
-        while (str[i] != '\0')
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
 
-        {
-        _putchar(str[i]);
-        i += 2;
-        }
-
-        _putchar('\n');
+	_putchar('\n');
 }
