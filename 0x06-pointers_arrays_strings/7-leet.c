db72a083b4d393076f894f8a860ee602153095b3
Tossie23 <tossiechr@gmail.com>
2023-07-27 04:04:01 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,25 +7,25 @@
  * Return: A pointer to the encoded string.
  */
 
-char *leet(char *s)
+char *leet(char *str)
 {
 	int count = 0, i;
 	int low_letters[] = {97, 101, 111, 116, 108};
 	int upp_letters[] = {65, 69, 79, 84, 76};
 	int numbers[] = {52, 51, 48, 55, 49};
 
-	while (*(s + count) != '\0')
+	while (*(str + count) != '\0')
 	{
 		for (i = 0; i < 5; i++)
 		{
-			if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
+			if (*(str + count) == low_letters[i] || *(str + count) == upp_letters[i])
 			{
-				*(s + count) = numbers[i];
+				*(str + count) = numbers[i];
 				break;
 			}
 		}
 	count++;
 	}
 
-	return (s);
+	return (str);
 }
