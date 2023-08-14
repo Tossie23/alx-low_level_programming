043d457d36a0bf82a3531cc27516f08a29bc780f
Tossie23 <tossiechr@gmail.com>
2023-08-14 01:29:16 -0700

    3-puts.c

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * _puts - Prints a message using the puts function
+ * @str: The string to be printed
+ */
+void _puts(char *str);
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
