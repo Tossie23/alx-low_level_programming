3c8eefb0094c3cfceb5e8a30aa508663107cc738
Tossie23 <tossiechr@gmail.com>
2023-08-17 04:27:44 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,17 +30,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	concat_len = len1 + n;
 
 	concatenated = malloc((concat_len + 1) * sizeof(char));
-	if (concatenated == NULL) {
+	if (concatenated == NULL)
 		return (NULL);
-	}
 
-	for (i = 0; i < len1; i++) {
+	for (i = 0; i < len1; i++)
 		concatenated[i] = s1[i];
-	}
 
-	for (j = 0; j < n; j++) {
+	for (j = 0; j < n; j++)
 		concatenated[i++] = s2[j];
-	}
 
 	concatenated[i] = '\0';
 
