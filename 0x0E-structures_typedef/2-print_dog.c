6ac1133d3fdcdbb1f1c549c0addea5a8437a3cec
Tossie23 <tossiechr@gmail.com>
2023-08-17 02:19:47 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * init_dog - initialize a variable of type struct dog
@@ -12,10 +13,28 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
+	if (d == NULL || name == NULL || owner == NULL)
 	{
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	/* Handle the error if any of the pointers are NULL */
+	return;
 	}
+
+	/* Allocate memory for the name and owner strings */
+	d->name = malloc(strlen(name) + 1);
+	d->owner = malloc(strlen(owner) + 1);
+
+	if (d->name == NULL || d->owner == NULL)
+	{
+	/* Handle the error if memory allocation fails */
+	free(d->name);
+	free(d->owner);
+	return;
+	}
+
+	/* Copy the name and owner strings */
+	strcpy(d->name, name);
+	strcpy(d->owner, owner);
+
+	/* Assign the age */
+	d->age = age;
 }
